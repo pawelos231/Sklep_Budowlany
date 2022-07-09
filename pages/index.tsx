9d8a6e070088dba8f07be2668328827dd47d8830
pawelLinek <pawellinek3D@gmail.com>
2022-07-09 17:25:55 +0200

    added redux toolkit packet and with counter test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,45 @@
-import type { NextPage } from 'next'
-import Header from '../components/Header/Header'
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "../lib/hooks";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  selectCount,
+} from "../actions/counter";
 
-const Home: NextPage = () => {
+const IndexPage: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const count = useAppSelector(selectCount);
+
+  const [incrementAmount, setIncrementAmount] = useState<number>(0);
+
   return (
-    <div>
-      <Header/>
-    </div>
-  )
-}
+    <>
+      <h1 className="text-red-500 text-lg">
+        Yee testy
+      </h1>
+      <h2 className="bg bg-violet-50 text-center">
+        The current number is
+        {count}
+      </h2>
+      <div className="flex bg-slate-200 justify-center transition 0.2s hover:bg-violet-500">
+        <input
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(Number(e.target.value))}
+          type="number"
+        />
+        <button
+          onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
+        >
+          Increment by amount
+        </button>
+      </div>
+      <div>
+        <button onClick={() => dispatch(decrement())}>Decrement by 1</button>
+        <button onClick={() => dispatch(increment())}>Increment by 1</button>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default IndexPage;
